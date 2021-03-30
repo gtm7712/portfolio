fe6eff424f02e6ad358a037a41c7425656f6a1e8
Giovanni Melchionne <gtm7712@rit.edu>
2021-03-29 20:08:51 -0400

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
-import './App.css';
+import './App.scss';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="sidebar">
+                <NavBar />
+            </div>
+            <div className="main-content">
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
